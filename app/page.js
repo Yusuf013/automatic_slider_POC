444d69d6b7c97f1399cf00880b0777431db42d30
Yusuf013 <146951398+Yusuf013@users.noreply.github.com>
2024-12-02 13:03:39 +0100

    Titel aangepast

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import { InfiniteLogoScroll } from '../app/components/page'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="mb-8 text-4xl font-bold">We worked with</h1>
       <InfiniteLogoScroll />
     </main>
   )
